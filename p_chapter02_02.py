0b2314128193c06aeb6a3ee3ee540f7304d7d5b1
yeongbin-gong <youngbin816@gmail.com>
2023-01-09 14:20:15 +0900

    클래스&메소드 심화(2-2)

--- p_chapter02_02.py
+++ p_chapter02_02.py
@@ -10,15 +10,23 @@ class Car():
     Date: 2023.1.9
     """
 
+    # 클래스 변수(모든 인스턴스가 공유)
+    car_count = 0
+
     def __init__(self, company, details):
         self._company = company
         self._details = details
+        Car.car_count += 1
 
     def __str__(self):
         return 'str : {} - {}'.format(self._company, self._details)
 
     def __repr__(self):
         return 'repr : {} - {}'.format(self._company, self._details)
+
+    def __del__(self):
+        print('del?')
+        Car.car_count -= 1
 
     def detail_info(self):
         print('Current ID : {}'.format(id(self)))
@@ -55,8 +63,33 @@ print()
 
 # 실행
 car1.detail_info()
+Car.detail_info(car1)
 car2.detail_info()
+Car.detail_info(car2)
+
+# 에러
+# Car.detail_info()
 
 # 비교
 print(car1.__class__, car2.__class__)
 print(id(car1.__class__), id(car2.__class__), id(car3.__class__))
+
+
+# 공유확인
+print(car1.car_count)
+print(car2.car_count)
+print(car1.__dict__)
+print(car2.__dict__)
+print(dir(car1))
+
+# 접근
+print(car1.car_count)
+print(Car.car_count)
+
+del car2
+# 삭제 확인
+print(car1.car_count)
+print(Car.car_count)
+
+# 인스턴스 네임스페이스에 없으면 상위에서 검색
+# 즉, 동일한 이름으로 변수 생성 가능(인스턴스 검색 후 -> 상위(클래스 변수, 부모클래스 변수))
